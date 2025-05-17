fc7ec856a5ebd3b6c78b352da52f340989fc073c
lbc0841 <cracker04012008@gmail.com>
2025-05-17 21:34:36 +0800

    feat: post description

--- src/scripts/load_page.js
+++ src/scripts/load_page.js
@@ -2,13 +2,18 @@
 
 window.addEventListener("load", function () {
 	const loading = document.getElementById("loading-anim");
-    const slotContainer = this.document.getElementById("page-content");
+    const pageContent = this.document.getElementById("page-content");
 
 	this.setTimeout(function(){
 
 		if(loading) loading.style.display = "none";
-        if(slotContainer) slotContainer.style.visibility = "visible";
-	}, 1000);
+
+        if(pageContent){
+			pageContent.style.animation = "page-content-appear 0.2s ease-out";
+			pageContent.style.opacity = "1";
+		} 
+
+	}, 600);
 });
 
 
