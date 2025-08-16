0c4c32267ad51720052284856cc5be0d21840b30
lbc0841 <cracker04012008@gmail.com>
2025-08-16 19:35:56 +0800

    feat: cursor style

--- src/scripts/cursor_particle.js
+++ src/scripts/cursor_particle.js
@@ -1,40 +1,35 @@
+// cursor particle
 const body = document.body;
-body.addEventListener("mousemove", function (e) {
-    let rect = body.getBoundingClientRect();
-    let x = e.clientX - rect.left;
-    let y = e.clientY - rect.top;
+let rect = body.getBoundingClientRect();;
+let mouseX = window.innerWidth / 2;
+let mouseY = window.innerHeight / 2;
 
-    creatParticles(x, y);
+body.addEventListener("mousemove", function (e) {
+    mouseX = e.clientX - rect.left;
+    mouseY = e.clientY - rect.top;
 });
 
-function creatParticles(x, y) {
-    const size = Math.floor(Math.random() * 5) + 2;
-    const offsetX = Math.floor(Math.random() * 20) - 10;
-    const offsetY = Math.floor(Math.random() * 20) - 10;
+function createParticle(x, y) {
+    const size = Math.floor(Math.random() * 3) + 3;
+    const offsetX = Math.floor(Math.random() * 20);
+    const offsetY = Math.floor(Math.random() * 20);
 
-    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    svg.setAttribute("width", size);
-    svg.setAttribute("height", size);
-    svg.classList.add("cursor-particle");
-    svg.style.left = (x+offsetX) + "px";
-    svg.style.top = (y+offsetY) + "px";
+    let particle = document.createElement("div");
+    particle.classList.add("cursor-particle");
+    particle.style.left = (x+offsetX) + "px";
+    particle.style.top = (y+offsetY) + "px";
+    particle.style.width = size + "px";
+    particle.style.height = size + "px";
+    particle.style.background = "#0ff";
 
-    const square = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-    square.setAttribute("x", "0");      // 左上角 x 座標
-    square.setAttribute("y", "0");      // 左上角 y 座標
-    square.setAttribute("width", size); // 正方形邊長
-    square.setAttribute("height", size);
-    square.setAttribute("fill", "white");
-
-    // let particle = document.createElement("div");
-    // particle.classList.add("cursor-particle");
-    // particle.style.left = x + "px";
-    // particle.style.top = y + "px";
-
-    svg.appendChild(square);
-    body.appendChild(svg);
+    // svg.appendChild(square);
+    body.appendChild(particle);
 
     setTimeout(() => {
-        svg.remove();
-    }, 250);
+        particle.remove();
+    }, 900);
 }
+
+setInterval(() => {
+    createParticle(mouseX, mouseY);
+}, 100);
